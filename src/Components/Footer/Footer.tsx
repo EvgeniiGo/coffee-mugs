41824b4ae94629b0d62880f8827ff3c981ba991c
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2023-04-04 17:32:48 +0300

    Footer component

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import logo from "../../images/logo.png";
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__info">
+        <img src={logo} alt="CoffeeStyle." className="footer__logo" />
+        <p className="footer__text">
+          Delivering the best coffee life since 1996. From coffee geeks to the
+          real ones.
+        </p>
+        <p className="footer__copy">CoffeeStyle Inc. &copy; 2023</p>
+      </div>
+      <div className="footer__column">
+        <h3 className="footer__column-title">Menu</h3>
+        <nav className="footer__links">
+          <NavLink className="footer__link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="footer__link" to="/products">
+            Our products
+          </NavLink>
+          <NavLink className="footer__link" to="/blog">
+            Blog
+          </NavLink>
+          <NavLink className="footer__link" to="/about">
+            About
+          </NavLink>
+          <NavLink className="footer__link" to="/contact">
+            Contact
+          </NavLink>
+        </nav>
+      </div>
+      <div className="footer__column">
+        <h3 className="footer__column-title">Follow us</h3>
+        <nav className="footer__links">
+          <a className="footer__link" href="https://www.facebook.com/">
+            Facebook
+          </a>
+          <a className="footer__link" href="https://www.instagram.com/">
+            Instagram
+          </a>
+          <a className="footer__link" href="https://www.pinterest.com/">
+            Pinterest
+          </a>
+          <a className="footer__link" href="https://twitter.com/">
+            Twitter
+          </a>
+        </nav>
+      </div>
+      <div className="footer__column">
+        <h3 className="footer__column-title">Contact us</h3>
+        <p className="footer__text">We're Always Happy to Help</p>
+        <a className="footer__email" href="mailto:us@coffeestyle.io">
+          us@coffeestyle.io
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
