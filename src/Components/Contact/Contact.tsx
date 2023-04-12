f0a261d6168091d51fb8afc4a662ed111adb07f8
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2023-04-12 15:41:32 +0300

    Offices component

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import "./Contact.css";
+import Intro from "../Intro/Intro";
+import Offices from "../Offices/Offices";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Intro
+        title="Let's Connect"
+        subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis."
+        label="OUR OFFICES"
+      />
+      <div className="contact__container">
+        <Offices />
+      </div>
+    </>
+  );
 };
 
 export default Contact;
---
+++ src/Components/Offices/Offices.tsx
@@ -0,0 +1,56 @@
+import "./Offices.css";
+
+const imgLondon: string = new URL(
+  `../../images/offices/london.jpg`,
+  import.meta.url
+).href;
+const imgCalifornia: string = new URL(
+  `../../images/offices/california.jpg`,
+  import.meta.url
+).href;
+
+const Offices = () => {
+  return (
+    <section className="offices">
+      <div className="office">
+        <img src={imgLondon} alt="office" className="office__image" />
+        <div className="office__info">
+          <p className="office__country-label">United Kingdom</p>
+          <h3 className="office__city-title">Canary Wharf, London</h3>
+          <p className="office__adress">
+            Jubilee Place London
+            <br />
+            E14
+            <br /> 5NY
+          </p>
+          <p className="office__opening-label">Opening times</p>
+          <div className="office__opening-times">
+            <p className="office__opening-time">Mon - Fri 08:00 to 22:00</p>
+            <p className="office__opening-time">Sat - 09:00 to 20:00</p>
+            <p className="office__opening-time">Sun - 12:00 to 18:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="office">
+        <img src={imgCalifornia} alt="office" className="office__image" />
+        <div className="office__info">
+          <p className="office__country-label">United States</p>
+          <h3 className="office__city-title">Venice Beach, California</h3>
+          <p className="office__adress">
+            9219 Old Kingston Street South
+            <br /> El Monte, CA
+            <br /> 91733
+          </p>
+          <p className="office__opening-label">Opening times</p>
+          <div className="office__opening-times">
+            <p className="office__opening-time">Mon - Wed 09:00 to 21:00</p>
+            <p className="office__opening-time">Thu - Sat 09:00 to 22:00</p>
+            <p className="office__opening-time">Sun - 10:00 to 19:00</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Offices;
