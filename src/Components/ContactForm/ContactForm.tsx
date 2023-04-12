11ee18da20eeb60b7731928d311ceb8f6318735e
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2023-04-12 21:37:26 +0300

    ContactForm component

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -1,9 +1,27 @@
 import { FormEvent } from "react";
 import "./ContactForm.css";
+import messageIcon from "../../images/contact-message-icon.png";
 
 const ContactForm = () => {
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const form = document.querySelector(".contact-form") as HTMLInputElement;
+
+    const button = document.querySelector(
+      ".subscription__button"
+    ) as HTMLButtonElement;
+
+    const message = document.querySelector(
+      ".form-section__success-message"
+    ) as HTMLParagraphElement;
+
+    button.textContent = "Please wait...";
+
+    setTimeout(() => {
+      form.style.display = "none";
+      message.style.display = "flex";
+    }, 1000);
   };
 
   return (
@@ -21,14 +39,16 @@ const ContactForm = () => {
             type="text"
             className="contact-form__input"
             placeholder="James Coffee"
+            required
           />
           <label htmlFor="" className="contact-form__label">
             Email address
           </label>
           <input
-            type="text"
+            type="email"
             className="contact-form__input"
             placeholder="customer@coffeestyle.io"
+            required
           />
           <label htmlFor="" className="contact-form__label">
             Your message
@@ -38,11 +58,20 @@ const ContactForm = () => {
             name="message"
             id="message"
             placeholder="Hi! I would like to ask something about mugs."
+            required
           />
           <button className="contact-form__button" type="submit">
             Send message
           </button>
         </form>
+        <div className="form-section__success-message">
+          <img
+            src={messageIcon}
+            alt="confirmation"
+            className="form-section__message-icon"
+          />
+          Thank you! Your submission has been received!
+        </div>
       </div>
       <div className="form-section__contact-column">
         <div className="form-section__info">
--- src/Components/Subscription/Subscription.tsx
+++ src/Components/Subscription/Subscription.tsx
@@ -1,34 +1,30 @@
 import "./Subscription.css";
 import messageIcon from "../../images/message-icon.png";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { FormEvent } from "react";
 
 const Subscription = () => {
-  const [email, setEmail] = useState<string>("");
-
-  function onEmailChange(e: ChangeEvent<HTMLInputElement>) {
-    setEmail(e.target.value);
-    console.log(email);
-  }
-
-  function onSubmit(e: FormEvent): void {
+  function onSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault();
-    if (email !== "") {
-      const input = document.querySelector(
-        ".subscription__input"
-      ) as HTMLInputElement;
-      const button = document.querySelector(
-        ".subscription__button"
-      ) as HTMLButtonElement;
-      const message = document.querySelector(
-        ".subscription__success-message"
-      ) as HTMLParagraphElement;
-      button.textContent = "Please wait...";
-      setTimeout(() => {
-        input.style.display = "none";
-        button.style.display = "none";
-        message.style.display = "flex";
-      }, 1000);
-    }
+
+    const input = document.querySelector(
+      ".subscription__input"
+    ) as HTMLInputElement;
+
+    const button = document.querySelector(
+      ".subscription__button"
+    ) as HTMLButtonElement;
+
+    const message = document.querySelector(
+      ".subscription__success-message"
+    ) as HTMLParagraphElement;
+
+    button.textContent = "Please wait...";
+
+    setTimeout(() => {
+      input.style.display = "none";
+      button.style.display = "none";
+      message.style.display = "flex";
+    }, 1000);
   }
 
   return (
@@ -41,11 +37,10 @@ const Subscription = () => {
       <h2 className="subscription__title">Coffee Updates</h2>
       <form action="" className="subscription__form" onSubmit={onSubmit}>
         <input
-          type="text"
+          type="email"
           className="subscription__input"
           placeholder="customer@coffeestyle.io"
-          value={email}
-          onChange={onEmailChange}
+          required
         />
         <button className="subscription__button" type="submit">
           Subscribe
